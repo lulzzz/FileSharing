3aa1649c9c3353d36bebf00120b8e8db286dbb21
Truong Chau Hien <truongchauhien@gmail.com>
2019-09-23 14:34:13 +0700

    Add ByePacket: UdpPacket.

---
+++ FileSharing.Commons/UdpPackets/ByePacket.cs
@@ -0,0 +1,22 @@
+﻿using FileSharing.Sockets.Packets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSharing.Commons.UdpPackets
+{
+    public class ByePacket : UdpPacket
+    {
+        public ByePacket(): base((byte) FileServerOpCode.Bye)
+        {
+
+        }
+
+        public ByePacket(UdpPacket udpPacket): base (udpPacket)
+        {
+
+        }
+    }
+}
