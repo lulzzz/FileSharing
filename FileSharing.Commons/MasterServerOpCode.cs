058abcf959443e4b53aed4945f7a80c6ca4e2aa5
Truong Chau Hien <truongchauhien@gmail.com>
2019-09-24 01:13:04 +0700

    Add MasterServerOpCode enum.

---
+++ FileSharing.Commons/MasterServerOpCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSharing.Commons
+{
+    public enum MasterServerOpCode : byte
+    {
+        RequestFileList,
+        ReturnFileList
+    }
+}
