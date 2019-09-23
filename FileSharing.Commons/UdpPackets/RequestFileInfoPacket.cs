2c7ab089aa99d9d270839e795cfa2c44e06a4815
Truong Chau Hien <truongchauhien@gmail.com>
2019-09-23 14:34:49 +0700

    Add RequestFileInfoPacket: UdpPacket.

---
+++ FileSharing.Commons/UdpPackets/RequestFileInfoPacket.cs
@@ -0,0 +1,45 @@
+﻿using FileSharing.Sockets.Packets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSharing.Commons.UdpPackets
+{
+    public class RequestFileInfoPacket : UdpPacket
+    {
+
+        public string FileName
+        {
+            get
+            {
+                using (var reader = base.GetPayloadBufferReader())
+                {
+                    return reader.ReadString();
+                }
+            }
+            set
+            {
+                using (var writer = base.GetPayloadBufferWriter())
+                {
+                    writer.BaseStream.SetLength(0L);
+                    writer.Write(value);
+                }
+            }
+        }
+
+        public RequestFileInfoPacket(string fileName) : base((byte)FileServerOpCode.RequestFileInfo)
+        {
+            using (var writer = base.GetPayloadBufferWriter())
+            {
+                writer.Write(fileName);
+            }
+        }
+
+        public RequestFileInfoPacket(UdpPacket udpPacket) : base(udpPacket)
+        {
+
+        }
+    }
+}
