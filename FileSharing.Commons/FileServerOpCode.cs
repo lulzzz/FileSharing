4d8d72b7d42277d2059c1c5263c1c4aa3597b0e6
Truong Chau Hien <truongchauhien@gmail.com>
2019-09-23 14:35:20 +0700

    Add FileServerOpCode enum.

---
+++ FileSharing.Commons/FileServerOpCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSharing.Commons
+{
+    public enum FileServerOpCode : byte
+    {
+        Ack,
+        Bye,
+        RequestFileInfo,
+        ReturnFileInfo,
+        RequestBlock,
+        ReturnBlock,
+    }
+}
