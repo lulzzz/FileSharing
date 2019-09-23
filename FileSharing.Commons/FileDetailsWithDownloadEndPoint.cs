98b60f740ce04e77c7ba8c79cfe25cc05ea3fb96
Truong Chau Hien <truongchauhien@gmail.com>
2019-09-24 01:11:22 +0700

    Add FileDetailsWithDownloadEndPoint.

---
+++ FileSharing.Commons/FileDetailsWithDownloadEndPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSharing.Commons
+{
+    public class FileDetailsWithDownloadEndPoint : FileDetails
+    {
+        public string DownloadIP { get; set; }
+
+        public int DownloadPort { get; set; }
+    }
+}
