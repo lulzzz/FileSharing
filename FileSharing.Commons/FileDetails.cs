978c21f963be85e5305ba7b22a54047a8d61c650
Truong Chau Hien <truongchauhien@gmail.com>
2019-09-24 01:11:05 +0700

    Add FileDetails.

---
+++ FileSharing.Commons/FileDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSharing.Commons
+{
+    public class FileDetails
+    {
+        public string Name { get; set; }
+
+        public long Size { get; set; }
+
+        public byte[] SHA512Hash { get; set; } // 512 bits, 64 bytes.
+    }
+
+}
