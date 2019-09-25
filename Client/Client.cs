eff2c062869abe27e3c2d3aa442e65cf3d0763c0
Truong Chau Hien <truongchauhien@gmail.com>
2019-09-26 04:22:13 +0700

    Add Client procedures.

---
+++ Client/Client.cs
@@ -0,0 +1,100 @@
+﻿using FileSharing.Client.Clients;
+using FileSharing.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Client
+    {
+        private string masterServerIP;
+        private int masterServerPort;
+
+        private List<FileDetailsWithDownloadEndPoint> fileList = new List<FileDetailsWithDownloadEndPoint>();
+
+        public Client(string masterServerIP, int masterServerPort)
+        {
+            this.masterServerIP = masterServerIP;
+            this.masterServerPort = masterServerPort;
+        }
+
+        public async Task GetFileList()
+        {
+            var masterServiceClient = new MasterServiceClient(new MasterServiceClientSettings()
+            {
+                MasterServerIP = this.masterServerIP,
+                MasterServerPort = this.masterServerPort
+            });
+
+            masterServiceClient.Start();
+            await masterServiceClient.MasterServer.RequestFileList();
+            masterServiceClient.Stop();
+            masterServiceClient.FileListReceived += (sender, args) =>
+            {
+                this.fileList = args.FileList;
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Thông báo: Đã nhận được danh sách tập tin.");
+                Console.ResetColor();
+            };
+        }
+
+        public async Task DownloadFile(int index)
+        {
+            FileDetailsWithDownloadEndPoint fileDetails;
+            try
+            {
+                fileDetails = this.fileList[index - 1];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Cảnh báo: Chỉ mục của tập tin vượt khoảng hợp lệ.");
+                Console.ResetColor();
+
+                return;
+            }
+
+            var fileServiceClient = new FileServiceClient(new FileServiceClientSettings()
+            {
+                FileName = fileDetails.Name,
+                FileServerIP = fileDetails.DownloadIP,
+                FileServerPort = fileDetails.DownloadPort
+            });
+
+            try
+            {
+                await fileServiceClient.Download();
+            } catch (UnknownFileException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Thông báo: Tập tin {fileDetails.Name} không tồn tại trên File Server.");
+                Console.ResetColor();
+                return;
+            } catch (CorruptedFileException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Thông báo: Tập tin {fileDetails.Name} không toàn vẹn trong quá trình truyền tải.");
+                Console.ResetColor();
+                return;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Thông báo: Đã tải xong tập tin {fileDetails.Name}.");
+            Console.ResetColor();
+        }
+
+        public void PrintFileList()
+        {
+            int index = 1;
+            Console.WriteLine($"{"Index",5}|{"File Name",32}");
+            foreach (var file in this.fileList)
+            {
+                Console.WriteLine($"{index++,5}|{file.Name,32}");
+            }
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,102 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            EnterLoop();
+        }
+
+        static string GetMasterServerIP()
+        {
+            IPAddress ipAddress;
+            string inputIP;
+            do
+            {
+                Console.Write("Master Server IP: ");
+                inputIP = Console.ReadLine();
+            } while (!IPAddress.TryParse(inputIP, out ipAddress));
+
+            return ipAddress.ToString();
+        }
+
+        static int GetMasterServerPort()
+        {
+            int port;
+            string inputPort;
+            do
+            {
+                Console.Write("Master Server Port: ");
+                inputPort = Console.ReadLine();
+            } while (!int.TryParse(inputPort, out port));
+
+            return port;
+        }
+
+        static int InputIndex()
+        {
+            int num;
+            string input;
+            do
+            {
+                Console.WriteLine("Chỉ mục của tập tin cần tải: ");
+                input = Console.ReadLine();
+            } while (!int.TryParse(input, out num));
+
+            return num;
+        }
+
+        static void EnterLoop()
+        {
+            string masterServerIP = GetMasterServerIP();
+            int masterServerPort = GetMasterServerPort();
+            var client = new Client(masterServerIP, masterServerPort);
+
+            ConsoleKeyInfo pressedKey;
+            do
+            {
+                Console.WriteLine("1. Lấy danh sách tập tin từ Master Server.");
+                Console.WriteLine("2. In danh sách tập tin đang lưu trữ");
+                Console.WriteLine("3. Tải tập tin");
+                Console.WriteLine("q. Thoát");
+                pressedKey = Console.ReadKey(true);
+
+                switch (pressedKey.Key)
+                {
+                    case ConsoleKey.D1:
+                        client.GetFileList().ContinueWith(task =>
+                        {
+                            task.Exception.Handle((ex) =>
+                            {
+                                if (ex is SocketException)
+                                {
+                                    var socketException = ex as SocketException;
+                                    switch (socketException.NativeErrorCode)
+                                    {
+                                        case 10061:
+                                            Console.ForegroundColor = ConsoleColor.Red;
+                                            Console.WriteLine("Lỗi: Không kết nối được tới Master Server.");
+                                            Console.ResetColor();
+                                            return true;
+                                        default:
+                                            return false;
+                                    }
+                                }
+                                return false;
+                            });
+                        }, TaskContinuationOptions.OnlyOnFaulted);
+                        break;
+                    case ConsoleKey.D2:
+                        client.PrintFileList();
+                        break;
+                    case ConsoleKey.D3:
+                        int index = InputIndex();
+                        client.DownloadFile(index).ContinueWith(task => { }, TaskContinuationOptions.OnlyOnFaulted);
+                        break;
+                    default:
+                        break;
+                }
+            } while (pressedKey.Key != ConsoleKey.Q);
         }
     }
 }
